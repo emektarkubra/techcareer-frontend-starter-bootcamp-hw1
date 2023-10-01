d76702fd64acd5c4dafacca8e3399ddad0cb71a5
Kübra Emektar <emektarrkubra@gmail.com>
2023-10-01 21:17:54 +0300

    edited

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -9,39 +9,39 @@ export default function Awards() {
                 <h2 className="mb-5">Awards &amp; Certifications</h2>
                 <ul className="fa-ul mb-0">
                     <li>
-                        <span className="fa-li"><FaTrophy /></span>
+                        <span className="fa-li trophy"><FaTrophy /></span>
                         Google Analytics Certified Developer
                     </li>
                     <li>
-                        <span className="fa-li"><FaTrophy /></span>
+                        <span className="fa-li trophy"><FaTrophy /></span>
                         Mobile Web Specialist - Google Certification
                     </li>
                     <li>
-                        <span className="fa-li"><FaTrophy /></span>
+                        <span className="fa-li trophy"><FaTrophy /></span>
                         1
                         <sup>st</sup>
                         Place - University of Colorado Boulder - Emerging Tech Competition 2009
                     </li>
                     <li>
-                        <span className="fa-li"><FaTrophy /></span>
+                        <span className="fa-li trophy"><FaTrophy /></span>
                         1
                         <sup>st</sup>
                         Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
                     </li>
                     <li>
-                        <span className="fa-li"><FaTrophy /></span>
+                        <span className="fa-li trophy"><FaTrophy /></span>
                         2
                         <sup>nd</sup>
                         Place - University of Colorado Boulder - Emerging Tech Competition 2008
                     </li>
                     <li>
-                        <span className="fa-li"><FaTrophy /></span>
+                        <span className="fa-li trophy"><FaTrophy /></span>
                         1
                         <sup>st</sup>
                         Place - James Buchanan High School - Hackathon 2006
                     </li>
                     <li>
-                        <span className="fa-li"><FaTrophy /></span>
+                        <span className="fa-li trophy"><FaTrophy /></span>
                         3
                         <sup>rd</sup>
                         Place - James Buchanan High School - Hackathon 2005
