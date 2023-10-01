febe2bde282818c0f21e798aee0b11c2efba7539
Kübra Emektar <emektarrkubra@gmail.com>
2023-10-01 17:50:29 +0300

    added Experience component

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,11 +1,14 @@
 import "./style/pageLayout.scss";
 
 import About from "./About";
+import Experience from "./Experience";
 
 export default function PageLayout() {
   return (
     <div class="container-fluid p-0">
       <About />
+      <hr />
+      <Experience />
       <hr />
     </div>
   );
