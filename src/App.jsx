f2680f9fcd6462b97ab28b5f1e87aedd28be76d1
Kübra Emektar <emektarrkubra@gmail.com>
2023-10-01 14:56:06 +0300

    edited Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar"
+import PageLayout from "./components/PageLayout"
 
 function App() {
 
   return (
-    <>
+    <div style={{display : "flex"}}>
     <Navbar />
-    </>
+    
+    {/* <PageLayout /> */}
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import "./style/navbar.scss"
 import { BsList } from "react-icons/bs";
+import { useState } from "react";
 
 export default function Navbar() {
+    const [isVisible, setIsVisible] = useState(false)
     return (
         <>
             <nav className="navbar" id="sideNav">
@@ -12,11 +14,11 @@ export default function Navbar() {
                             <img className="img-profile" src="/src/assets/profile.jpg" />
                         </span>
                     </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <button onClick={() => setIsVisible(prev => !prev)} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                         <BsList className="icon" />
                     </button>
                 </div>
-                <div className="navbar-collapse collapse" id="navbarResponsive">
+                <div className={`navbar-collapse collapse ${isVisible ? null : `visible`}`} id="navbarResponsive">
                     <ul className="navbar-nav">
                         <li className="nav-item active"><a className="nav-link js-scroll-trigger active" href="#about">About</a></li>
                         <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
